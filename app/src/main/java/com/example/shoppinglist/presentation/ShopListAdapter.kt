285c1fc4e49fb24a43f26de94867e4c5cdb18cb0
SlavaMostovoi <mossla@inbox.ru>
2024-10-06 16:26:31 +0300

    created ShopList recycler with base configuration

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.shopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+
+    class shopItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): shopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return shopItemViewHolder(view)
+    }
+
+    override fun getItemCount() = shopList.size
+
+    override fun onBindViewHolder(holder: shopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        holder.tvName.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+        holder.itemView.setOnLongClickListener {
+            true
+        }
+    }
+}
